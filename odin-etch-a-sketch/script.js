8ffde2d9afcd2eb79cff788daab546d4e42aafbe
Edgar <edgargarteong@gmail.com>
2024-03-01 18:22:05 +0800

    added js ui to rps and added etchasketch files

---
+++ odin-etch-a-sketch/script.js
@@ -0,0 +1,5 @@
+const body = document.body;
+for (let i = 0; i < 10; i++){
+    const div = document.createElement("div"); 
+    body.appendChild(div);
+}
--- odin-rockpaperscissors/rockpaperscissors.js
+++ odin-rockpaperscissors/rockpaperscissors.js
@@ -31,7 +31,7 @@ function playRound(playerSelection, computerSelection){
                 }
     }
 }
-
+/*
 function playGame(){
     let playerScore = 0; 
     let computerScore = 0; 
@@ -59,5 +59,91 @@ function playGame(){
         console.log("It's a draw!");
     }
 }
+*/
+ 
+let scibutton = document.createElement("button"); 
+scibutton.className = "scissorsbutton"; 
+scibutton.textContent = "Scissors!"; 
 
-playGame();
+let rockbutton = document.createElement("button"); 
+rockbutton.className = "rockbutton"; 
+rockbutton.textContent = "Rock!"; 
+
+let paperbutton = document.createElement("button"); 
+paperbutton.className = "paperbutton";
+paperbutton.textContent = "Paper!"; 
+
+let body = document.body; 
+let br1 = document.createElement("br"); 
+let br2 = document.createElement("br"); 
+body.appendChild(br1);
+body.appendChild(br2);
+body.appendChild(rockbutton); 
+body.appendChild(paperbutton); 
+body.appendChild(scibutton);
+
+
+let playerScore = 0;
+let computerScore = 0; 
+scibutton.addEventListener("click", (e) => {
+    let res = playRound("Scissors", getComputerChoice());
+    if (res["result"] == 1) {
+        playerScore++; 
+    } else if (res["result"] == 2) {
+        computerScore++;
+    }
+    console.log(e.target);
+    console.log(res["output"]);
+    let output = document.createElement("div"); 
+    output.textContent = `${res["output"]}`;
+    body.appendChild(output);
+    if (playerScore == 5){
+        console.log("Player wins!");
+        alert("Player wins!");
+    } else if (computerScore == 5){
+        console.log("Computer wins");
+        alert("Computer wins!")
+    }
+})
+
+paperbutton.addEventListener("click", (e) => {
+    let res = playRound("Paper", getComputerChoice());
+    if (res["result"] == 1) {
+        playerScore++; 
+    } else if (res["result"] == 2) {
+        computerScore++;
+    }
+    console.log(e.target);
+    console.log(res["output"]);
+    let output = document.createElement("div"); 
+    output.textContent = `${res["output"]}`;
+    body.appendChild(output);
+    if (playerScore == 5){
+        console.log("Player wins!");
+        alert("Player wins!");
+    } else if (computerScore == 5){
+        console.log("Computer wins");
+        alert("Computer wins!")
+    }
+})
+
+rockbutton.addEventListener("click", (e) => {
+    let res = playRound("Rock", getComputerChoice());
+    if (res["result"] == 1) {
+        playerScore++; 
+    } else if (res["result"] == 2) {
+        computerScore++;
+    }
+    console.log(e.target);
+    console.log(res["output"]);
+    let output = document.createElement("div"); 
+    output.textContent = `${res["output"]}`;
+    body.appendChild(output);
+    if (playerScore == 5){
+        console.log("Player wins!");
+        alert("Player wins!");
+    } else if (computerScore == 5){
+        console.log("Computer wins");
+        alert("Computer wins!")
+    }
+})
