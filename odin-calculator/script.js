3def3a06fc6f408deb93d41c0b1743fb944c7ec0
Edgar <edgargarteong@gmail.com>
2024-03-05 15:14:49 +0800

    convert to scientific style calculator, added backspace, decimal and parentheses buttons

--- odin-calculator/script.js
+++ odin-calculator/script.js
@@ -1,9 +1,8 @@
 /*
 functionalities to add: 
-1. convert into scientific calculator style 
+1. fix formatting for display text
 2. put button functions into functions to add into keypress 
 3. complete keyboard functionality
-4. add backspace button
 */ 
 
 //variables to help with calculations
@@ -11,9 +10,11 @@ let num1 = null;
 let num2 = null; 
 let operator = "";
 let result = null;
-let displaynums = "";
+let inputhold = "";
+let outputhold = "";
+let decimalflag = false;
 
-//operation functions
+//requirement functions
 function add(num1, num2){
     return num1 + num2;
 }
@@ -48,77 +49,69 @@ function operate(operator, num1, num2){
 
 //const for use throughout
 const BODY = document.body; 
-const NUMTEXT = BODY.querySelector(".numtext");
-const OPERATORTEXT = BODY.querySelector(".operatortext");
+const INPUTTEXT = BODY.querySelector(".inputtext");
+const OUTPUTTEXT = BODY.querySelector(".outputtext");
+const ALLNUMBUTTONS = BODY.querySelectorAll(".numberbutton"); 
+const NUMBUTTONS = [...ALLNUMBUTTONS]; 
+const CLEARBUTTON = BODY.querySelector('#clearbutton'); 
+const LEFTBRACKET = BODY.querySelector('#leftbracketbutton');
+const RIGHTBRACKET = BODY.querySelector('#rightbracketbutton');
+const BACKSPACE = BODY.querySelector("#backspacebutton"); 
+const PLUS = BODY.querySelector('#plusbutton'); 
+const MINUS = BODY.querySelector('#minusbutton');
+const MULTIPLY = BODY.querySelector('#multiplybutton'); 
+const DIVIDE = BODY.querySelector('#dividebutton'); 
+const EQUALS = BODY.querySelector('#equalsbutton');
+const DECIMAL = BODY.querySelector('#decimalbutton');
 
-//keyboard functionality 
-//consider wrapping the functionality in functions so the keypress can just call it
-document.addEventListener("keydown", (e) => {
-    console.log(e);
-    if (e.key == "1"){
-        displaynums = displaynums + "1";
-        NUMTEXT.textContent = displaynums.substring(displaynums.length - 9);
-    } else if (e.key == "2"){
-        displaynums = displaynums + "2";
-        NUMTEXT.textContent = displaynums.substring(displaynums.length - 9);
-    } else if (e.key == "3"){
-        displaynums = displaynums + "3";
-        NUMTEXT.textContent = displaynums.substring(displaynums.length - 9);
-    } else if (e.key == "4"){
-        displaynums = displaynums + "4";
-        NUMTEXT.textContent = displaynums.substring(displaynums.length - 9);
-    } else if (e.key == "5"){
-        displaynums = displaynums + "5";
-        NUMTEXT.textContent = displaynums.substring(displaynums.length - 9);
-    } else if (e.key == "6"){
-        displaynums = displaynums + "6";
-        NUMTEXT.textContent = displaynums.substring(displaynums.length - 9);
-    } else if (e.key == "7"){
-        displaynums = displaynums + "7";
-        NUMTEXT.textContent = displaynums.substring(displaynums.length - 9);
-    } else if (e.key == "8"){
-        displaynums = displaynums + "8";
-        NUMTEXT.textContent = displaynums.substring(displaynums.length - 9);
-    } else if (e.key == "9"){
-        displaynums = displaynums + "9";
-        NUMTEXT.textContent = displaynums.substring(displaynums.length - 9);
-    } else if (e.key == "0"){3
-        displaynums = displaynums + "0";
-        NUMTEXT.textContent = displaynums.substring(displaynums.length - 9);
-    }
-})
 
 //number buttons functionality 
-const TEMP = BODY.querySelectorAll(".numberbutton"); 
-const NUMBUTTONS = [...TEMP]; 
 NUMBUTTONS.forEach(button => {
     button.addEventListener("click", () => {
-        displaynums = displaynums + button.textContent;
-        NUMTEXT.textContent = displaynums.substring(displaynums.length - 9);
+        inputhold = inputhold + button.textContent;
+        INPUTTEXT.textContent = inputhold.substring(inputhold.length - 24);
     })
 })
 
+//parentheses functionality 
+
+//decimal functionality
+DECIMAL.addEventListener("click", () => {
+    if (!decimalflag){
+        inputhold = inputhold + DECIMAL.textContent;
+        INPUTTEXT.textContent = inputhold.substring(inputhold.length - 24);
+        decimalflag = true;
+    } 
+})
+
+
 //clear button functionality 
-const CLEARBUTTON = BODY.querySelector('#clearbutton'); 
 CLEARBUTTON.addEventListener("click", () => {
-    NUMTEXT.textContent = "";
-    OPERATORTEXT.textContent = "";
-    displaynums = "";
+    INPUTTEXT.textContent = "";
+    OUTPUTTEXT.textContent = "";
     num1 = null; 
     num2 = null; 
     operator = "";
+    result = null;
+    inputhold = "";
+    outputhold = "";
+    decimalflag = false;
 })
 
+//backspace functionality
+BACKSPACE.addEventListener("click", () => {
+
+})
+
+
 //plus button functionality 
-const PLUSBUTTON = BODY.querySelector('#plusbutton'); 
-PLUSBUTTON.addEventListener("click", () => {
-    OPERATORTEXT.textContent = '+';
+PLUS.addEventListener("click", () => {
     if (!num1){
-        num1 = Number(displaynums); 
-        NUMTEXT.textContent = "";
-        displaynums = ""
-    } else if (num1 && displaynums != "") {
-        num2 = Number(displaynums);
+        num1 = Number(displayhold); 
+        DISPLAY = "";
+        displayhold = ""
+    } else if (num1 && displayhold != "") {
+        num2 = Number(displayhold);
         operate(num1, num2);
         num1 = result; 
         num2 = null; 
@@ -127,57 +120,50 @@ PLUSBUTTON.addEventListener("click", () => {
     operator = "+";
 })
 
-//minus button functionality
-const MINUSBUTTON = BODY.querySelector('#minusbutton'); 
-MINUSBUTTON.addEventListener("click", () => {
-    OPERATORTEXT.textContent = '-';
+//minus button functionality 
+MINUS.addEventListener("click", () => {
     if (!num1){
-        num1 = Number(displaynums); 
-    } else if (num1 && displaynums != "") {
-        num2 = Number(displaynums);
+        num1 = Number(displayhold); 
+    } else if (num1 && displayhold != "") {
+        num2 = Number(displayhold);
         operate(num1, num2);
         num1 = result; 
         num2 = null; 
     }
     operator = "-";
-    displaynums = ""; 
-    NUMTEXT.textContent = "";
+    displayhold = ""; 
+    DISPLAY = "";
 })
 
 
 //multiply button functionality
-const MULTIPLYBUTTON = BODY.querySelector('#multiplybutton'); 
-MULTIPLYBUTTON.addEventListener("click", () => {
-    OPERATORTEXT.textContent = '×';
+MULTIPLY.addEventListener("click", () => {
     if (!num1){
-        num1 = Number(displaynums); 
-    } else if (num1 && displaynums != "") {
-        num2 = Number(displaynums);
+        num1 = Number(displayhold); 
+    } else if (num1 && displayhold != "") {
+        num2 = Number(displayhold);
         operate(num1, num2);
         num1 = result; 
         num2 = null; 
     }
     operator = "×";
-    displaynums = ""; 
-    NUMTEXT.textContent = "";
+    displayhold = ""; 
+    DISPLAY = "";
 })
 
 //divide button functionality
-const DIVIDEBUTTON = BODY.querySelector('#dividebutton'); 
-DIVIDEBUTTON.addEventListener("click", () => {
+DIVIDE.addEventListener("click", () => {
     if (!num1){
-        num1 = Number(displaynums); 
+        num1 = Number(displayhold); 
         operator = "÷";
-        displaynums = ""; 
-        NUMTEXT.textContent = ""; 
-        OPERATORTEXT.textContent = "÷";
-    } else if (num1 && displaynums != "") {
-        num2 = Number(displaynums);
+        displayhold = ""; 
+        DISPLAY = ""; 
+    } else if (num1 && displayhold != "") {
+        num2 = Number(displayhold);
         operate(num1, num2);
         if (result == "ERROR"){
-            NUMTEXT.textContent = result;
-            OPERATORTEXT.textContent = "";
-            displaynums = ""; 
+            DISPLAY = result;
+            displayhold = ""; 
             num1 = null; 
             num2 = null;
             operator = "";
@@ -186,39 +172,41 @@ DIVIDEBUTTON.addEventListener("click", () => {
             operator = "÷";
             num1 = result; 
             num2 = null; 
-            displaynums = "";
-            NUMTEXT.textContent = ""; 
-            OPERATORTEXT.textContent = "÷";
+            displayhold = "";
+            DISPLAY = ""; 
         } 
     } else {
-        displaynums = ""; 
-        NUMTEXT.textContent = ""; 
-        OPERATORTEXT.textContent = "÷";
+        displayhold = ""; 
+        DISPLAY = ""; 
     }
 })
 
 //equals button functionality 
 //consider putting the equals function into the operator buttons since it 'equals' if there is a second number
 //alternatively, you can consider doing the functionality like a scientific calculator
-const EQUALS = BODY.querySelector('#equalsbutton');
 EQUALS.addEventListener("click", () => {
     if (num1 != null) {
-        if (displaynums != ""){
-            num2 = Number(displaynums);
+        if (displayhold != ""){
+            num2 = Number(displayhold);
             operate(operator, num1, num2);  
-            OPERATORTEXT.textContent = "";
                 if (result != "ERROR"){
                     num1 = result;
                 } else {
                     num1 = null;
                 }
-            NUMTEXT.textContent = String(result);
-            displaynums = "";
+            DISPLAY = String(result);
+            displayhold = "";
             num2 = null; 
             operator = "";
             result = null; 
         } else {
-            NUMTEXT.textContent = String(num1);
+            DISPLAY = String(num1);
         }
     }
+})
+
+//keyboard functionality 
+//consider wrapping the functionality in functions so the keypress can just call it
+document.addEventListener("keydown", (e) => {
+    console.log(e);
 })
