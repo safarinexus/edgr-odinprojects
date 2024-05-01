09584dfd939d4167d88f849e799b217085cfd883
Edgar <edgargarteong@gmail.com>
2024-05-01 12:36:54 +0800

    added preliminary react files

---
+++ odin-react-cvapplication/src/components/Display.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+function Display() {
+
+}
+
+export default Display;
---
+++ odin-react-cvapplication/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Display from './components/Display'
+
+import './styles/index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Display />
+  </React.StrictMode>,
+)
