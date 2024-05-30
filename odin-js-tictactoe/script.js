01c11f3f29b182c56b342a01c610f4fa0de7e461
Edgar <edgargarteong@gmail.com>
2024-05-30 18:42:41 +0800

    finished tictactoe

--- odin-js-tictactoe/script.js
+++ odin-js-tictactoe/script.js
@@ -38,8 +38,6 @@ const gameBoard = (function() {
     }
 })();
 
-
-
 //Player factory function to create player objects
 function player(name, character) {
     return {
@@ -49,22 +47,35 @@ function player(name, character) {
 }
 
 
-
 //Game Controller IIFE to create an instance of a controller for the game
-const gameEngine = (function() {
+const game = (function() {
 
     const board = gameBoard.getBoard();
+    let turn = 0;
+
 
     const playerInit = (p1name, p2name) => {
         return [player(p1name, "x"), player(p2name, "o")];
     }
 
-    const players = playerInit(); 
-    const playerCount = 2;
-    const turns = 5; 
+    const getTurn = () => {
+        return turn;
+    }
 
-    const validMove = (row, col) => {
-        if (row < 0 || row > 2 || col < 0 || col > 2) {
+    const changeTurn = () => {
+        if (turn === 0) {
+            turn = 1;
+        } else {
+            turn = 0;
+        }
+    }
+
+    const resetTurn = () => {
+        turn = 0; 
+    }
+
+    const validMove = (players, row, col) => {
+        if ((row < 0 || row > 2 || col < 0 || col > 2) || (isWin(players) !== "Next Turn")) {
             return false;
         } else {
             if (board[row][col] !== 0) {
@@ -75,7 +86,7 @@ const gameEngine = (function() {
         }
     }
     
-    const isWin = () => {  
+    const isWin = (players) => {  
         if ((board[0][0] === "x" && board[0][1] === "x" && board[0][2] === "x")||
         (board[1][0]=== "x" && board[1][1] === "x" && board[1][2] === "x")||
         (board[2][0] === "x" && board[2][1] === "x" && board[2][2] === "x")||
@@ -103,69 +114,99 @@ const gameEngine = (function() {
         }
     }
 
-    const startGame = () => {
-        for (let i = 0; i < turns; i++) {
-            for (let j = 0; j < playerCount; j++) {
-                //connect with announcer display
-    
-                let row = -1;
-                let column = -1;
-                
-                while (validMove(row, column) === false) {
-                    row = prompt("Which row? (1 - 3): ") - 1; 
-                    column = prompt("Which column? (1 - 3): ") - 1; 
-                    if (validMove(row, column) === false) {
-                        console.log("Please enter a valid move!");
-                    }
-                }
-    
-                board[row][column] = players[j].getCharacter();
-                //announcer isWin
-                if (isWin() !== "Next Turn") {
-                    return;
-                }
-            }
-        }
-    }
-
     return {
-        players, 
-        startGame
+        isWin, 
+        validMove,  
+        playerInit,
+        getTurn, 
+        changeTurn, 
+        resetTurn 
     }
 })(); 
+
+
 
 const displayController = (function() {
     const PLAYERCARDSECTION = document.querySelector(".playercard-section"); 
     const NAMEPROMPT = document.querySelector("#nameprompt");
     const ENTER = document.querySelector("#enter");
     const XPLAYER = document.querySelector("#xname");
-    const OPLAYER = document.querySelector("#oname");
-    const CELL1 = document.querySelector("#cell1");
-    const CELL2 = document.querySelector("#cell2");
-    const CELL3 = document.querySelector("#cell3");
-    const CELL4 = document.querySelector("#cell4");
-    const CELL5 = document.querySelector("#cell5");
-    const CELL6 = document.querySelector("#cell6");
-    const CELL7 = document.querySelector("#cell7");
-    const CELL8 = document.querySelector("#cell8");
-    const CELL9 = document.querySelector("#cell9");
+    const OPLAYER = document.querySelector("#oname");   
+    const TURNSECTION = document.querySelector(".turn-section");
+    const ANNOUNCER = document.querySelector(".announcer");
+    const GRID = document.querySelector(".tictactoegrid");
+
+    let players;
+
+    const board = gameBoard.getBoard();
+
+    const renderBoard = () => {
+        GRID.innerHTML = ""; 
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board[i].length; j++) {
+                const temp = j + 1 + i*3;
+                if (board[i][j] === "x") {
+                    GRID.innerHTML += "<div id='cell" + temp + "' class='tictactoecell'><svg width='100' height='100'><line x1='0' y1='0' x2='100' y2='100' stroke='black' stroke-width='10' /><line x1='100' y1='0' x2='0' y2='100' stroke='black' stroke-width='10' /></svg></div>";
+                } else if (board[i][j] === "o") {
+                    GRID.innerHTML += "<div id='cell" + temp + "' class='tictactoecell'><svg width='100' height='100' xmlns='http://www.w3.org/2000/svg'><circle cx='50' cy='50' r='40' stroke='black' stroke-width='10' fill='transparent'/></svg></div>";           
+                } else if (board[i][j] === 0) {
+                    GRID.innerHTML += "<div id='cell" + temp + "' class='tictactoecell'></div>";
+                }
+            }    
+        }
+
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board[i].length; j++) {
+                const temp = j + 1 + i*3;
+                document.querySelector("#cell" + temp).addEventListener("click", () => {
+                    if (game.validMove(players, i, j)) {
+                        board[i][j] = players[game.getTurn()].getCharacter(); 
+                        if (renderBoard()) {
+                            game.changeTurn(); 
+                            announcerCall(players[game.getTurn()].getName() + "'s turn...");
+                        }
+                    }
+                })
+            }
+        }
+
+        const result = game.isWin(players);
+        if (result !== "Next Turn") {
+            console.log(result);
+            announcerCall(result);
+            setTimeout(() => alert(result), 100);
+            setTimeout(() => {if (confirm("Play Again?")) {
+                gameBoard.resetBoard();
+                game.resetTurn(); 
+                announcerCall(players[game.getTurn()].getName() + "'s turn...");
+                renderBoard();
+            }}, 200)
+            return false;
+        }
+
+        return true;
+    }
+
+    const announcerCall = (string) => {
+        ANNOUNCER.innerHTML = string; 
+    }
 
     ENTER.addEventListener("click", (e) => {
         if (XPLAYER.value !== "" && OPLAYER.value !== ""){
             e.preventDefault();
+            players = game.playerInit(XPLAYER.value, OPLAYER.value);
             PLAYERCARDSECTION.innerHTML = "<div class='playercard' id='player1'><p>Player 1: " + XPLAYER.value + "</p><svg width='20' height='20'><line x1='0' y1='0' x2='20' y2='20' stroke='black' stroke-width='3'/><line x1='20' y1='0' x2='0' y2='20' stroke='black' stroke-width='3'/></svg></div>";
             PLAYERCARDSECTION.innerHTML += "<div class='playercard' id='player2'><p>Player 2: " + OPLAYER.value + "</p><svg width='20' height='20' xmlns='http://www.w3.org/2000/svg'><circle cx='10' cy='10' r='8' stroke='black' stroke-width='3' fill='transparent'/></svg></div>";
             NAMEPROMPT.close(); 
+            announcerCall(players[game.getTurn()].getName() + "'s turn...");
         }
     })
 
-    CELL8.addEventListener("click", () => {
-        CELL8.innerHTML = "<svg width='100' height='100'><line x1='0' y1='0' x2='100' y2='100' stroke='black' stroke-width='10' /><line x1='100' y1='0' x2='0' y2='100' stroke='black' stroke-width='10' /></svg>"; 
-    })
-    CELL9.addEventListener("click", () => {
-        CELL9.innerHTML = "<svg width='100' height='100' xmlns='http://www.w3.org/2000/svg'><circle cx='50' cy='50' r='40' stroke='black' stroke-width='10' fill='transparent'/></svg>"; 
-    })
+    renderBoard();
 
     NAMEPROMPT.showModal(); 
-    return;
+
+    return {
+        renderBoard
+    }
 })();
