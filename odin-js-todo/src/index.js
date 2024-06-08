8535c5567be1b875373d4cfe49d941d134387918
Edgar <edgargarteong@gmail.com>
2024-06-08 16:37:00 +0800

    added preliminary tasks to todo

--- odin-js-todo/src/index.js
+++ odin-js-todo/src/index.js
@@ -1,14 +1,32 @@
 console.log('initiated');
 
+import { formatDistance, subDays } from "date-fns";
+
+formatDistance(subDays(new Date(), 3), new Date(), { addSuffix: true });
+
 import './style.css';
+import Logo from "./icons/logo.svg"
 import TodayIcon from './icons/today.svg';
 import UpcomingIcon from './icons/upcoming.svg';
 import newProjIcon from './icons/newproject.svg' 
+//todo class
+//project class
+//crud project 
+//crud todos
+//dom functions for project
+//dom functions for tasks
+//function to sort by today
+//function to sort by upcoming
 
+const LOGO = document.querySelector('#toplogo');
 const TODAY = document.querySelector('#today');
 const UPCOMING = document.querySelector('#upcoming');
 
 //adding all images
+const TopLogo = new Image(); 
+TopLogo.src = Logo;
+LOGO.prepend(TopLogo);
+
 const Today = new Image(); 
 Today.src = TodayIcon; 
 TODAY.prepend(Today);
---
+++ odin-js-todo/src/modules/todos.js
@@ -0,0 +1,5 @@
+export default function todos(project, title, date, description, dueDate, priority) {
+    if (project) {
+        
+    }
+}
