516302e7c8a5a716b08c07363169bf674f9e0f47
Edgar <edgargarteong@gmail.com>
2024-06-19 14:31:12 +0800

    finished extra dropdown menu and image carousel

--- extras/dynamic-user-interfaces/index.js
+++ extras/dynamic-user-interfaces/index.js
@@ -15,3 +15,123 @@ DROPDOWN.addEventListener("mouseout", () => {
     MENU.style.visibility = 'hidden';
     MENU.style.opacity = '0';
 })
+
+const NEXT = document.querySelector('#next');
+const PREV = document.querySelector("#prev");
+const PIC1 = document.querySelector('#pic1');
+const PIC2 = document.querySelector('#pic2');
+const PIC3 = document.querySelector('#pic3');
+const PIC4 = document.querySelector('#pic4');
+const PIC5 = document.querySelector('#pic5');
+const PICS = document.querySelector('#pictures');
+let position = 0;
+PIC1.style.backgroundColor = 'rgb(255, 255, 255, 0.7)'; 
+
+function firstButton() { 
+    if (position !== 0) {
+        position = 0;
+        PICS.style.left = position + 'px';
+        PIC1.style.backgroundColor = 'rgb(255, 255, 255, 0.7)';
+        PIC2.style.backgroundColor = 'rgb(255, 255, 255, 0.2)';
+        PIC3.style.backgroundColor = 'rgb(255, 255, 255, 0.2)';
+        PIC4.style.backgroundColor = 'rgb(255, 255, 255, 0.2)';
+        PIC5.style.backgroundColor = 'rgb(255, 255, 255, 0.2)';
+
+    }
+}
+function secondButton() {
+    if (position !== -500) {
+        position = -500;
+        PICS.style.left = position + 'px';
+        PIC1.style.backgroundColor = 'rgb(255, 255, 255, 0.2)';
+        PIC2.style.backgroundColor = 'rgb(255, 255, 255, 0.7)';
+        PIC3.style.backgroundColor = 'rgb(255, 255, 255, 0.2)';
+        PIC4.style.backgroundColor = 'rgb(255, 255, 255, 0.2)';
+        PIC5.style.backgroundColor = 'rgb(255, 255, 255, 0.2)';
+    }
+}
+
+function thirdButton() {
+    if (position !== -1000) {
+        position = -1000;
+        PICS.style.left = position + 'px';
+        PIC1.style.backgroundColor = 'rgb(255, 255, 255, 0.2)';
+        PIC2.style.backgroundColor = 'rgb(255, 255, 255, 0.2)';
+        PIC3.style.backgroundColor = 'rgb(255, 255, 255, 0.7)';
+        PIC4.style.backgroundColor = 'rgb(255, 255, 255, 0.2)';
+        PIC5.style.backgroundColor = 'rgb(255, 255, 255, 0.2)';
+    }
+}
+
+function fourthButton() {
+    if (position !== -1500) {
+        position = -1500;
+        PICS.style.left = position + 'px';
+        PIC1.style.backgroundColor = 'rgb(255, 255, 255, 0.2)';
+        PIC2.style.backgroundColor = 'rgb(255, 255, 255, 0.2)';
+        PIC3.style.backgroundColor = 'rgb(255, 255, 255, 0.2)';
+        PIC4.style.backgroundColor = 'rgb(255, 255, 255, 0.7)';
+        PIC5.style.backgroundColor = 'rgb(255, 255, 255, 0.2)';
+    }
+}
+
+function fifthButton() {
+    if (position !== -2000) {
+        position = -2000;
+        PICS.style.left = position + 'px';
+        PIC1.style.backgroundColor = 'rgb(255, 255, 255, 0.2)';
+        PIC2.style.backgroundColor = 'rgb(255, 255, 255, 0.2)';
+        PIC3.style.backgroundColor = 'rgb(255, 255, 255, 0.2)';
+        PIC4.style.backgroundColor = 'rgb(255, 255, 255, 0.2)';
+        PIC5.style.backgroundColor = 'rgb(255, 255, 255, 0.7)';
+    }
+}
+
+
+NEXT.addEventListener("click", () => {
+    if (position !== -2000) {
+        if (position === 0) {
+            secondButton();
+        } else if (position === -500) {
+            thirdButton();
+        } else if (position === -1000) {
+            fourthButton(); 
+        } else if (position === -1500) {
+            fifthButton(); 
+        }
+    }
+})
+
+PREV.addEventListener("click", () => {
+    if (position !== 0) {
+        if (position === -2000) {
+            fourthButton();
+        } else if (position === -1500) {
+            thirdButton();
+        } else if (position === -1000) {
+            secondButton(); 
+        } else if (position === -500) {
+            firstButton();
+        }
+    }
+})  
+
+PIC1.addEventListener("click", firstButton)
+PIC2.addEventListener("click", secondButton)
+PIC3.addEventListener("click", thirdButton)
+PIC4.addEventListener("click", fourthButton)
+PIC5.addEventListener("click", fifthButton)
+
+
+let arr = [firstButton, secondButton, thirdButton, fourthButton, fifthButton] 
+let counter = 1; 
+setInterval(() => {
+    console.log(arr[counter].name + ' called.');
+    arr[counter]();
+    if (counter === 4) {
+        counter = 0;
+    } else {
+        counter += 1;
+    }
+}, 5000)
+
