57aec2051b86e5edd8e223f2db0f67324f4c1aeb
Edgar <edgargarteong@gmail.com>
2024-08-23 20:35:07 +0800

    reverted to before dotenvx

--- odin-node-express-messageboard/db/pool.js
+++ odin-node-express-messageboard/db/pool.js
@@ -1,22 +1,5 @@
 const { Pool } = require("pg"); 
 
-if (process.env.MODE === "dev") {
-    console.log("dev in use");
-    module.exports = new Pool({
-        host: "localhost", 
-        user: "edgr", 
-        database: "messages", 
-        password: "lightning",
-        port: 5432,
-    });
-} else if (process.env.MODE === "prod") { 
-    console.log("prod in use");
-    module.exports = new Pool({
-        host: process.env.DATABASE_HOST,
-        database: process.env.DATABASE_NAME,
-        username: process.env.DATABASE_USER,
-        password: process.env.DATABASE_OTHER_PASSWORD,
-        ssl: 'require',
-    });
-}
-
+module.exports = new Pool({
+    connectionString: process.env.DATABASE_URI, 
+})
