7b0bb9db2da99fddb2c96e6bb4be74f653203b8a
Edgar <edgargarteong@gmail.com>
2024-11-08 20:30:10 +0800

    fix crashed deployment

--- odin-node-express-postgres-inventory/db/pool.js
+++ odin-node-express-postgres-inventory/db/pool.js
@@ -2,5 +2,6 @@ const { Pool } = require("pg");
 
 module.exports = new Pool ({
     connectionString: process.env.CONNECTION_STRING,
+    //uncomment below for prod, comment for dev
     ssl: true,
 })
